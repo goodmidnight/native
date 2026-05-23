d79607d0cf8448dac2bffdb0b5dc711b76cb00e1
goodmidnight <fancythat0831@gmail.com>
2026-05-23 13:51:08 +0900

    build(android): integrate Google OSS Licenses plugin
    - Add `google-play-services-oss-licenses` library and `google-oss-licenses` plugin definitions to the version catalog.
    - Update `GooglePlugin` to apply the OSS licenses plugin and include the required play services dependency.
    - Register the `google.oss.licenses` plugin in the root build configuration.

--- android/build-system/src/main/java/io/goodmidnight/scanner/buildsystem/plugin/GooglePlugin.kt
+++ android/build-system/src/main/java/io/goodmidnight/scanner/buildsystem/plugin/GooglePlugin.kt
@@ -1,9 +1,19 @@
 package io.goodmidnight.scanner.buildsystem.plugin
 
+import io.goodmidnight.scanner.buildsystem.util.Extensions.versionCatalog
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class GooglePlugin : Plugin<Project> {
     override fun apply(target: Project) {
+        with(target) {
+            with(pluginManager) {
+                apply("com.google.android.gms.oss-licenses-plugin")
+            }
+            dependencies {
+                add("implementation", versionCatalog.findLibrary("google-play-services-oss-licenses").get())
+            }
+        }
     }
 }
